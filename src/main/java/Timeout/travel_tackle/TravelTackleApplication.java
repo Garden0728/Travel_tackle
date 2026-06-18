e5834df353ddfbde016dc293745f934bb6eb39b8
garden <113418319+Garden0728@users.noreply.github.com>
2026-06-18 20:21:52 +0900

    feat : first commit

---
+++ src/main/java/Timeout/travel_tackle/TravelTackleApplication.java
@@ -0,0 +1,13 @@
+package Timeout.travel_tackle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTackleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTackleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Timeout/travel_tackle/TravelTackleApplicationTests.java
@@ -0,0 +1,13 @@
+package Timeout.travel_tackle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTackleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
